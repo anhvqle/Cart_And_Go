989d169ca467ade4e85e72c56eded95fabe0b44e
Anh Le <43076815+anhvqle@users.noreply.github.com>
2020-12-22 16:02:59 +0700

    Update main.js

--- static/main.js
+++ static/main.js
@@ -11,3 +11,18 @@ jQuery(document).ready(function(){
         $("#" + inputID).val(quantity);
     });
 });
+
+function getRandomColor(){
+    var letters = "0123456789ABCDEF";
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
+
+function changeHeaderColor(){
+    header.style.color = getRandomColor();
+}
+
+setInterval("changeHeaderColor()",500);
